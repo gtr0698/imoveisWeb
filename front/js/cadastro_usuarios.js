67a857c47a8ecd245097770adac5a7fde70cd1aa
PedroHenrique <pedrodiedrich123@gmail.com>
2023-06-17 09:54:15 -0300

    front

--- front/js/cadastro_usuarios.js
+++ front/js/cadastro_usuarios.js
@@ -16,7 +16,7 @@ function gerar_cadastro(){
     div_content += '<input type="number" value="" name="idPessoa" id="idPessoa" style="display:none;">'
     div_content += '<div class="ui inverted form">';
     div_content += '<div class="two fields">';
-    div_content += '<div class="ten wide field">';
+    div_content += '<div class="two field">';
     div_content += '<label for="nome">Nome</label>';
     div_content += '<input type="text" placeholder="Nome Completo" name="nome" id="nome">'
     div_content += '</div>';    
@@ -25,7 +25,7 @@ function gerar_cadastro(){
     div_content += '<input type="number" placeholder="CPF ou CNJP" name="numeroDocumento" id="numeroDocumento">'
     div_content += '</div></div>';
     div_content += '<div class="two fields">';
-    div_content += '<div class="ten wide field">';
+    div_content += '<div class="two field">';
     div_content += '<label for="email">E-mail</label>';
     div_content += '<input type="email" inputmode="email" placeholder="exemplo@mail.com" id="email" name="email">'
     div_content += '</div>';
@@ -33,22 +33,22 @@ function gerar_cadastro(){
     // div_content += '<label for="dataNascimento">Data de Nascimento</label>';
     // div_content += '<input type="date" id="dataNascimento" name="dataNascimento">';
     // div_content += '</div>';
-    div_content += '<div class="six wide field">';
+    div_content += '<div class="two field">';
     div_content += '<label for="senha">Senha</label>';
     div_content += '<input type="password" placeholder="Senha" id="senha" name="senha">'
     div_content += '</div></div>';
     div_content += '<div class="two fields">';
-    div_content += '<div class="six wide field">';
+    div_content += '<div class="two field right">';
     div_content += '<label for="telefone">Telefone</label>';
     div_content += '<input type="number" id="telefone" name="telefone">';
     div_content += '</div>';
-    div_content += '<div class="six wide field">';
-    div_content += '<label for="tipoPessoa">Tipo</label>';
-    div_content += '<select id="tipoPessoa" name="tipoPessoa">';
-    div_content += '<option value=""></option>';
-    div_content += '<option value="PESSOA_FISICA">Pessoa Física</option>';
-    div_content += '<option value="PESSOA_JURIDICA">Pessoa Jurídica</option></select>';
-    div_content += '</div>';
+    // div_content += '<div class="two field">';
+    // div_content += '<label for="tipoPessoa">Tipo</label>';
+    // div_content += '<select id="tipoPessoa" name="tipoPessoa">';
+    // div_content += '<option value=""></option>';
+    // div_content += '<option value="PESSOA_FISICA">Pessoa Física</option>';
+    // div_content += '<option value="PESSOA_JURIDICA">Pessoa Jurídica</option></select>';
+    // div_content += '</div>';
    
     div.setAttribute('class', 'ui inverted segment');
     div.innerHTML = div_content;
